333e4eddc5ffec0685366a3f296cf995cdb35509
olicauli <olicauli@protonmail.com>
2022-08-10 23:02:34 -0500

    start adding webpack

--- src/game.js
+++ src/game.js
@@ -1,5 +1,7 @@
 //const { Phaser } = require("./phaser");
 
+import Phaser from 'phaser';
+
 //import { TestGame } from "testgamescene.js";
 
 import Preloader from './scenes/Preloader.js';
@@ -10,7 +12,7 @@ const config = {
     width: 800,
     height: 600,
     pixelArt: true,
-    parent: "game",
+    parent: 'game',
     physics: {
         default: 'arcade',
         arcade: {
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = 
+{
+    entry: './src/game.js',
+    output: 
+    {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
